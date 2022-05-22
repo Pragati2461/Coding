39208269e1a82232fa5c380bc1a8813c59cea946
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2022-05-22 15:51:17 +0530

    Second main commit

--- something.cpp
+++ something.cpp
@@ -8,12 +8,18 @@ class Something{
             cout << "Enter value : ";
             cin >> num1;
         }
+
+        void add2(){
+            num1 += 2;
+        }
 };
 
 int main(){
     Something s;
 
     s.set();
+
+    s.add2();
 
     cout << s.num1;
 
